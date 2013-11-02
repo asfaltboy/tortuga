6b99193bc6c4ccf84a5ccdd62709617abb94049a
Pavel Savchenko <pasha.savchenko@gmail.com>
2013-11-02 11:34:14 +0200

    Update UnRar plug-in to better match the logic

--- plugins/unrar.py
+++ plugins/unrar.py
@@ -4,16 +4,17 @@ A Plugin to unarchive given rar file into destination.
 depends on rarfile: http://rarfile.berlios.de/doc/
 and unrar.exe file (provided)
 """
-
+import logging
 import rarfile
+
+logger = logging.getLogger('plugins.{}'.format(__name__))
 
 
 class Command(object):
     rar_path = None
     dest_path = None
 
-    def __init__(self, rar_path, dest_path=None):
-        self.rar_path = rar_path
+    def __init__(self, dest_path=None):
         self.dest_path = dest_path
 
     # TODO
@@ -25,5 +26,8 @@ class Command(object):
         return False
 
     def run(self):
+        if not self.input:
+            logger.warn("A rar source path is required as input")
+            return
         rf = rarfile.RarFile(self.rar_path)
         rf.extractall(path=self.dest_path)
